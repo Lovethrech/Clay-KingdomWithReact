ffcb9e0951696ca993bcb26df1a1dbba404c4d1f
Lovethrech <rachealdolapo45@gmail.com>
2025-08-24 14:16:09 +0100

    ...starting the components.

--- src/components/items/ItemsList.component.tsx
+++ src/components/items/ItemsList.component.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import { ItemInterface } from '../../models/items/Item.interface'
 
-export const ItemsListComponent: React.FC<>
+export const ItemsListComponent: React.FC<{
+    items: ItemInterface[]
+}> = (props) =>{
+    return(
+        
+    )
+}
 
