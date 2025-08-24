bbdc577438a6f2b69b5dfb9bcff59e95570538ba
Lovethrech <rachealdolapo45@gmail.com>
2025-08-24 14:13:43 +0100

    ...starting the components.

--- src/components/items/ItemsList.component.tsx
+++ src/components/items/ItemsList.component.tsx
@@ -1,32 +1,5 @@
 import React from 'react'
 import { ItemInterface } from '../../models/items/Item.interface'
 
-export class ItemsListComponent extends React.Component<{
-    items: ItemInterface[]
-}> {
-    constructor(
-        props:{
-            items: any[]
-        }
-    ){
-        super(props)
-    }
-    render(): React.ReactNode{
-        const {items} = this.props
-        return <div>
-            <h3>
-                Items:
-            </h3>
-            <ul>
-                {
-                    items.map(
-                        (item, index) => {
-                            return <li key={index}>{item.name}</li>
-                        } 
-                    )
-                }
-            </ul>
-        </div>
-    }
 }
 
