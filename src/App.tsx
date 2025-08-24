4de319b509eebae2d3b2cefbb4d2b5e2c65d8617
Lovethrech <rachealdolapo45@gmail.com>
2025-08-24 14:26:05 +0100

    ...starting the components.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { ItemsListComponent } from "./components/items/ItemsList.component";
 const items: any[]=[
   {
     id: 1,
-    name: 'Item 1'
+    name: 'Item 1',
+    selected: false
   },
   {
     id: 2,
