a746f4aa75209c05cc5c91eabff22f812345336c
Lovethrech <rachealdolapo45@gmail.com>
2025-09-04 16:26:34 +0100

    ...working on all section component.

--- src/components/items/ItemsList.component.tsx
+++ src/components/items/ItemsList.component.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 // import { ItemInterface } from '../../models/items/Item.interface';
 
-export const ItemsListComponent: React.FC<{
-    items: ItemInterface[]
-}> = (props) =>{
-    return(
-        <div>
-            <h3>
-                Items:
-            </h3>
-            <ul>
-                {
-                    props.items.map(
-                        (item, index) => <li key={index}>{item.name}</li>
-                    )
-                }
-            </ul>
-        </div>
-    )
-}
+// export const ItemsListComponent: React.FC<{
+//     items: ItemInterface[]
+// }> = (props) =>{
+//     return(
+//         <div>
+//             <h3>
+//                 Items:
+//             </h3>
+//             <ul>
+//                 {
+//                     props.items.map(
+//                         (item, index) => <li key={index}>{item.name}</li>
+//                     )
+//                 }
+//             </ul>
+//         </div>
+//     )
+// }
