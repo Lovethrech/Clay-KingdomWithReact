f969222b2d3c49086cced6b0a8bf4445683cd5a1
Lovethrech <rachealdolapo45@gmail.com>
2025-09-05 17:33:32 +0100

    ...working on all section component.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const {personName, setPersonName}=React.useState('');
+  const [personName, setPersonName]=React.useState('');
 
   return(
-    <h1>Hello {personName}</h1>
-    <input type="text" onChange={(e)=>{}} />
+    <div className="app-main">
+      <h1>Hello {personName} </h1>
+      <input type="text" onChange={(e)=> setPersonName(e.target.value)} />
+    </div>
+    // 5366133327274050
   );
 }
 
