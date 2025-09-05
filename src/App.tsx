7cedf063efdb7cedeeb480f11e1494670048e7b6
Lovethrech <rachealdolapo45@gmail.com>
2025-09-05 17:14:19 +0100

    ...working on all section component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,6 @@
 import React from 'react';
 import './App.css';
-// import { ItemInterface } from "./models/items/Item.interface";
-// import { ItemsListComponent } from "./components/items/ItemsList.component";
 
-// const items: ItemInterface[]=[
-//   {
-//     id: 1,
-//     name: 'Item 1',
-//     selected: false
-//   },
-//   {
-//     id: 2,
-//     name: 'Item 2',
-//     selected: false
-//   },
-//   {
-//     id: 3,
-//     name: 'Item 3',
-//     selected: false
-//   }
-// ]
 
 function App() {
   return(
